6240c517f44292228a9043be27e4fa921a818cec
doronma <marcus_doron@yahoo.com>
2025-06-18 10:21:00 +0300

    add execution printout

--- python/hello.py
+++ python/hello.py
@@ -6,3 +6,13 @@ simple python file
 # a function that get user name and return hello 'name'
 def hello(name):
     return f"Hello {name}"
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+        result = hello(name)
+        print(result)
+    else:
+        print("Please provide a name as an argument")
